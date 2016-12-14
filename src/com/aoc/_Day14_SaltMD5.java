6f2b181cff5cc5f1b3fd067a0f76e8b244a5b26a
Akhila Raghavan <akhila.raghavan@tacton.com>
2016-12-14 23:40:21 +0100

    day 14

--- src/com/aoc/_Day14_SaltMD5.java
+++ src/com/aoc/_Day14_SaltMD5.java
@@ -4,38 +4,42 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 import javax.xml.bind.DatatypeConverter;
 
 public class _Day14_SaltMD5 {
+	private static final Pattern CONS_THREE  = Pattern.compile("(.)\\1\\1");
 	
 	public static void main(String args[]) throws Exception{
 		String salt = "zpqevtbw";
 		MessageDigest instance = MessageDigest.getInstance("MD5");
-		int index = 0;
+		AtomicInteger index = new AtomicInteger(0);
 		
-		for(int i = 0; i<Integer.MAX_VALUE; i++) {
+		IntStream.range(0, Integer.MAX_VALUE).allMatch(i -> {
 			byte[] byteArray= (salt + i).getBytes(StandardCharsets.UTF_8);
 			byte[] digest = instance.digest(byteArray);
 			String hex = DatatypeConverter.printHexBinary(digest).toLowerCase();
-			
 			hex = findHashFor(hex, instance, 0);
-			for(int j = 0; j<hex.length(); j++) {
-				if(j == hex.length() -2) break;
-				if(hex.charAt(j) == hex.charAt(j+1) && hex.charAt(j+1) == hex.charAt(j+2)) {
-					//System.out.println("found for " + i);
-					if(findFiveConsequtives(i +1, hex.charAt(j), salt, instance)) {
-						System.out.println("for " + i);
-						++index;
-					}
-					break;
+			Matcher matcher = CONS_THREE.matcher(hex);
+			
+			if(matcher.find()) {
+				//System.out.println("found for " + i);
+				String charToMatch = matcher.group() + matcher.group().substring(0, 2);
+				if(findFiveConsequtives(i +1, charToMatch, salt, instance)) {
+					System.out.println("for " + i);
+					index.incrementAndGet();
 				}
 			}
-			if(index == 64) {
+			if(index.get() == 64) {
 				System.out.println(i);
-				break;
+				return false;
 			}
-		}
+			return true;
+		});
 	}
 
 	private static String findHashFor(String hex, MessageDigest instance, int count) {
@@ -48,29 +52,14 @@ public class _Day14_SaltMD5 {
 	}
 
 	static Map<Integer, String> cache = new HashMap<>(); 
-	private static boolean findFiveConsequtives(int i, char charAt, String salt, MessageDigest instance) {
-		int end = i + 1000;
-		while (i <= end) {
+	private static boolean findFiveConsequtives(int index, String charToMatch, String salt, MessageDigest instance) {
+		return IntStream.range(index, index + 1000)
+		.anyMatch(i -> {
 			byte[] byteArray = (salt + i).getBytes(StandardCharsets.UTF_8);
 			byte[] digest = instance.digest(byteArray);
-			String hex = DatatypeConverter.printHexBinary(digest).toLowerCase();
-			if(!cache.containsKey(i)) {
-				cache.put(i, findHashFor(hex, instance, 0));
-			}
-			hex = cache.get(i); 
-			for (int j = 0; j < hex.length(); j++) {
-				if (j == hex.length() - 4)
-					break;
-				if(hex.charAt(j) == charAt) {
-					if (hex.charAt(j) == hex.charAt(j + 1) && hex.charAt(j + 1) == hex.charAt(j + 2)
-							&& hex.charAt(j + 2) == hex.charAt(j + 3) && hex.charAt(j + 3) == hex.charAt(j + 4)) {
-						return true;
-					}
-				}
-			}
-			++i;
-		}
-		return false;
+			final String hex = DatatypeConverter.printHexBinary(digest).toLowerCase();
+			return cache.computeIfAbsent(i, (key) -> findHashFor(hex, instance, 0)).contains(charToMatch);
+		});
 	}
 
 }
